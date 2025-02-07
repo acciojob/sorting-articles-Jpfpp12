e97e14215f8c8c01052846ff3b075d6d8d146172
Jpfpp12 <118419073+Jpfpp12@users.noreply.github.com>
2025-02-07 12:01:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+      const bands = [
+            'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 
+            'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 
+            'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+        ];
+
+        function stripArticle(name) {
+            return name.replace(/^(a |an |the )/i, '');
+        }
+
+        bands.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
